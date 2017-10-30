8b125952fac4f91116d571d1fbd1b3f4c2e90686
Wiliam Ferraciolli <wiliam334@hotmail.com>
2017-10-30 23:01:19 +0000

    Added a table with available rooms

--- linked-in-learning-full-stack-app-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
+++ linked-in-learning-full-stack-app-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
@@ -10,13 +10,15 @@ export class AppComponent implements OnInit {
 
   public submitted: boolean;
   roomsearch : FormGroup;
-
+  rooms: Room[];
 
   ngOnInit(){
     this.roomsearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
     });
+
+    this.rooms = ROOMS;
   }
 
   /**
@@ -28,9 +30,45 @@ export class AppComponent implements OnInit {
     console.log(value);
   }
 
+  /**
+   * Method to reserve a room by id.
+   * @param {string} value
+   */
+  reserveRoom(value: string){
+    console.log("Room id for reservation" + value);
+  }
+
 }
 
 export interface RoomSearch{
   checkin : string;
   checkout: string;
 }
+
+export interface Room{
+  id : string;
+  roomNumber: string;
+  price : string;
+  links: string;
+}
+
+var ROOMS : Room[] = [
+  {
+    "id": "37489234327",
+    "roomNumber": "406",
+    "price": "25",
+    "links": ""
+  },
+  {
+    "id": "84329874798",
+    "roomNumber": "407",
+    "price": "20",
+    "links": ""
+  },
+  {
+    "id": "17238423787",
+    "roomNumber": "408",
+    "price": "22",
+    "links": ""
+  }
+];
