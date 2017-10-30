7284ccbb6c725bb0012660fe2c497482ba273b1b
Wiliam Ferraciolli <wiliam334@hotmail.com>
2017-10-30 22:36:05 +0000

    Added a form to submit check ins

--- linked-in-learning-full-stack-app-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
+++ linked-in-learning-full-stack-app-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public submitted: boolean;
+  roomsearch : FormGroup;
+
+
+  ngOnInit(){
+    this.roomsearch = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    });
+  }
+
+  /**
+   * Method to track whether the form was submitted
+   * @param {RoomSearch} value
+   * @param {boolean} valid
+   */
+  onSubmit({value, valid} : {value:RoomSearch, valid: boolean}){
+    console.log(value);
+  }
+
+}
+
+export interface RoomSearch{
+  checkin : string;
+  checkout: string;
 }
