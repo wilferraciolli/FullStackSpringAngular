067b91df8e9f1aa77eb41c8eb9e5c08d880f25cf
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2021-10-22 16:45:11 +0100

    API - Added more posts

--- BlogAssignment/Blog-api/src/main/java/com/wiltech/blog/profile/ProfileRestService.java
+++ BlogAssignment/Blog-api/src/main/java/com/wiltech/blog/profile/ProfileRestService.java
@@ -1,12 +1,14 @@
 package com.wiltech.blog.profile;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/profile")
 @RequiredArgsConstructor
--- BlogAssignment/Blog-api/src/main/resources/db/migration/V1_002__Inserts.sql
+++ BlogAssignment/Blog-api/src/main/resources/db/migration/V1_002__Inserts.sql
@@ -1,8 +1,14 @@
 INSERT INTO post(id, user_id, title, content, creation_date_time)
-VALUES (2000, 1000, 'Post title', 'Post content', '2020-01-01T00:00:00');
+VALUES (2000, 1000, 'Post title', 'Post content', '2020-01-01T09:00:00');
 
 INSERT INTO post(id, user_id, title, content, creation_date_time)
-VALUES (2001, 1000, 'Post title Second', 'Post content Second', '2021-01-01T00:00:00');
+VALUES (2001, 1000, 'Post title Second', 'Post content Second', '2021-01-01T09:30:00');
+
+INSERT INTO post(id, user_id, title, content, creation_date_time)
+VALUES (2002, 1000, 'Post title Third', 'Post content Third', '2021-01-10T15:30:00');
+
+INSERT INTO post(id, user_id, title, content, creation_date_time)
+VALUES (2003, 1000, 'Post title Fourth', 'Post content Fourth', '2021-02-01T19:30:00');
 
 INSERT INTO post_tag(id, post_id, value)
 VALUES (3000, 2000, 'tag1');
@@ -21,3 +27,8 @@ INSERT INTO post_tag(id, post_id, value)
 VALUES (3006, 2001, 'tag4');
 INSERT INTO post_tag(id, post_id, value)
 VALUES (3007, 2001, 'tag5');
+
+INSERT INTO post_tag(id, post_id, value)
+VALUES (3008, 2003, 'tag6');
+INSERT INTO post_tag(id, post_id, value)
+VALUES (3009, 2003, 'tag7');
