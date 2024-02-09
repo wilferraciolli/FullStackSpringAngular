9325bd45869c7cb2e0d6ae74f4f99694a9819add
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-02-09 12:08:14 +0000

    LLM UI - Added client for chatbot

--- LLM AI App/LLM-AI-UI/src/app/app.component.ts
+++ LLM AI App/LLM-AI-UI/src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'LLM-AI-UI';
+export class AppComponent implements OnInit {
+
+  private _url: string = 'http://localhost:5001/api/chat';
+
+  private _httpClient: HttpClient = inject(HttpClient);
+  private _formBuilder: FormBuilder = inject(FormBuilder);
+
+  public messageResponse: AIMessageResponse | null = null;
+  public chatHistory: WritableSignal<ChatItem[]> = signal([]);
+
+  public myForm = this._formBuilder.group({
+    question: ['', [Validators.required]]
+  });
+
+  public ngOnInit(): void {
+    this.chatHistory.set([{ user: 'bot', message: 'Hi, I am a bot. How can I help you today?' }]);
+  }
+
+  public onSubmit(): void {
+    const questionToAsk: string | null | undefined = this.myForm?.value?.question;
+
+    if (questionToAsk !== null && questionToAsk !== undefined) {
+      this.chatHistory.mutate((current: ChatItem[]) => current.push({
+        user: 'user',
+        message: questionToAsk
+      }));
+
+      this.myForm?.reset();
+      this._sendMessage(questionToAsk);
+    }
+  }
+
+  private _sendMessage(message: string): void {
+    console.log('got message ', message);
+
+    this._httpClient.post<AIMessageResponse>(this._url, this._buildPayload(message)).subscribe(
+      (response: AIMessageResponse) => this.messageResponse = response,
+      error => this.chatHistory.mutate((current: ChatItem[]) => current.push({
+        user: 'bot',
+        message: error.message
+      })));
+  }
+
+  private _buildPayload(message: string): ChatRequest {
+    return {
+      userMsg: message,
+      newChatThread: true
+    };
+  }
 }
+
+export interface AIMessageResponse {
+  message: string;
+}
+
+export interface ChatRequest {
+  userMsg: string;
+  newChatThread: boolean;
+}
+
+export interface ChatItem {
+  user: 'user' | 'bot';
+  message: string;
+}
+
--- LLM AI App/LLM-AI-UI/src/app/app.module.ts
+++ LLM AI App/LLM-AI-UI/src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
