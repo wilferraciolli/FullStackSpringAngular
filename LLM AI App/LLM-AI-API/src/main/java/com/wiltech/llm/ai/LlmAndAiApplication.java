55f2e299fce925d638cee0a32be884a25de929e3
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-02-09 08:32:13 +0000

    LLM AI - initial commit

---
+++ LLM AI App/LLM-AI-API/src/main/java/com/wiltech/llm/ai/LlmAndAiApplication.java
@@ -0,0 +1,13 @@
+package com.wiltech.llm.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmAndAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmAndAiApplication.class, args);
+	}
+
+}
---
+++ LLM AI App/LLM-AI-API/src/test/java/com/wiltech/llm/ai/LlmAndAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiltech.llm.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmAndAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
