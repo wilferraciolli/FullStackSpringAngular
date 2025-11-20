f307d4fff69c80059fb52c2902dc9c0c7a4eef48
WilFerraciolli <wiliam334@hotmail.com>
2025-11-20 22:58:58 +0200

    org-chart-api : initial commit

---
+++ OrgChart/org-chart-api/src/main/java/com/wiltech/orgchart/OrgChartApiApplication.java
@@ -0,0 +1,13 @@
+package com.wiltech.orgchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgChartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgChartApiApplication.class, args);
+	}
+
+}
---
+++ OrgChart/org-chart-api/src/test/java/com/wiltech/orgchart/OrgChartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiltech.orgchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgChartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
