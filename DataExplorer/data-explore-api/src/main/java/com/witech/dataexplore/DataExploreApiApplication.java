c5713ed1a00181d34acfc1945cbf912aabcc8a9a
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2026-04-30 14:40:52 +0100

    data explore pir - initial commit

---
+++ DataExplorer/data-explore-api/src/main/java/com/witech/dataexplore/DataExploreApiApplication.java
@@ -0,0 +1,13 @@
+package com.witech.dataexplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExploreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataExploreApiApplication.class, args);
+	}
+
+}
---
+++ DataExplorer/data-explore-api/src/test/java/com/witech/dataexplore/DataExploreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.witech.dataexplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataExploreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
