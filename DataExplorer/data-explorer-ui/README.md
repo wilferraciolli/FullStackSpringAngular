302ceff05dd23abf39e4acb1b5a0add70436cee6
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2026-04-30 12:06:26 +0100

    data explorer ui - added toolbar

--- DataExplorer/data-explorer-ui/README.md
+++ DataExplorer/data-explorer-ui/README.md
@@ -4,3 +4,6 @@ Project used to allow dynamic query builder to gather data from API.
 It will allow users to create and save queries and to export it to csv files.
 
 
+## Dependencies
+
+### material
--- DataExplorer/data-explorer-ui/src/app/app.ts
+++ DataExplorer/data-explorer-ui/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIcon, MatIconButton],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('data-explorer-ui');
