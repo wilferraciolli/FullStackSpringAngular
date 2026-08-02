8d7156f5205b325d2f5e978760f6b5fe1d56da03
WilFerraciolli <wiliam334@hotmail.com>
2026-08-02 16:47:12 +0300

    file-analyzer - initial commit

---
+++ File-Analyzer/file-analyzer-api/src/main/java/com/wiltech/fileanalyzer/FileAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.wiltech.fileanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileAnalyzerApplication.class, args);
+	}
+
+}
---
+++ File-Analyzer/file-analyzer-api/src/test/java/com/wiltech/fileanalyzer/FileAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiltech.fileanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
